2d7ee5b3d1d1446d7079b4ea0575d6f2dd15c06e
Rhea Garud <rg921@ic.ac.uk>
2023-06-01 16:58:56 +0100

    Add list of people for search page and scrollview

--- frontend/App.js
+++ frontend/App.js
@@ -1,20 +1,187 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ScrollView } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ScrollView style={styles.scrollView}>
+      <View>
+        <View style={[styles.card, styles.shadowProp]}>
+          <View>
+            <Text style={styles.heading}>
+              Edward W.                                                 23
+            </Text>
+          </View>
+          <Text>
+            £1,500 - £2,300 pm
+          </Text>
+          <View style={styles.tag}>
+            <Text>
+              Student
+            </Text>  
+          </View>  
+        </View>
+  
+        <View style={[styles.card, styles.shadowProp]}>
+          <View>
+            <Text style={styles.heading}>
+              George C.                                                  21
+            </Text>
+          </View>
+          <Text>
+            £1,500 - £2,000 pm
+          </Text>
+          <View style={styles.tag}>
+            <Text>
+              Student
+            </Text>  
+          </View>
+        </View>
+  
+        <View style={[styles.card, styles.shadowProp]}>
+          <View>
+            <Text style={styles.heading}>
+              Maddie P.                                                  28
+            </Text>
+          </View>
+          <Text>
+            £1,600 - £2,200 pm
+          </Text>
+          <View style={styles.tag}>
+            <Text>
+              Student
+            </Text>  
+          </View>
+        </View>
+  
+        <View style={[styles.card, styles.shadowProp]}>
+          <View>
+            <Text style={styles.heading}>
+              Stuart G.                                                     23
+            </Text>
+          </View>
+          <Text>
+            £1,500 - £2,100 pm
+          </Text>
+          <View style={styles.tag}>
+            <Text>
+              Student
+            </Text>  
+          </View>
+        </View>
+  
+        <View style={[styles.card, styles.shadowProp]}>
+          <View>
+            <Text style={styles.heading}>
+              Stuart G.                                                     23
+            </Text>
+          </View>
+          <Text>
+            £1,500 - £2,100 pm
+          </Text>
+          <View style={styles.tag}>
+            <Text>
+              Student
+            </Text>  
+          </View>
+        </View>
+  
+        <View style={[styles.card, styles.shadowProp]}>
+          <View>
+            <Text style={styles.heading}>
+              Stuart G.                                                     23
+            </Text>
+          </View>
+          <Text>
+            £1,500 - £2,100 pm
+          </Text>
+          <View style={styles.tag}>
+            <Text>
+              Student
+            </Text>  
+          </View>
+        </View>
+  
+        <View style={[styles.card, styles.shadowProp]}>
+          <View>
+            <Text style={styles.heading}>
+              Stuart G.                                                     23
+            </Text>
+          </View>
+          <Text>
+            £1,500 - £2,100 pm
+          </Text>
+          <View style={styles.tag}>
+            <Text>
+              Student
+            </Text>  
+          </View>
+        </View>
+  
+        <View style={[styles.card, styles.shadowProp]}>
+          <View>
+            <Text style={styles.heading}>
+              Stuart G.                                                     23
+            </Text>
+          </View>
+          <Text>
+            £1,500 - £2,100 pm
+          </Text>
+          <View style={styles.tag}>
+            <Text>
+              Student
+            </Text>  
+          </View>
+        </View>
+  
+        <View style={[styles.card, styles.shadowProp]}>
+          <View>
+            <Text style={styles.heading}>
+              Stuart G.                                                     23
+            </Text>
+          </View>
+          <Text>
+            £1,500 - £2,100 pm
+          </Text>
+          <View style={styles.tag}>
+            <Text>
+              Student
+            </Text>  
+          </View>
+        </View>
+  
+      </View>
+    </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  heading: {
+    fontSize: 18,
+    fontWeight: '600',
+    marginBottom: 13,
+  },
+  card: {
+    backgroundColor: '#F5F5F5',
+    borderRadius: 100,
+    paddingVertical: 10,
+    paddingHorizontal: 25,
+    width: '100%',
+    marginVertical: 10,
+  },
+  shadowProp: {
+    shadowColor: '#171717',
+    shadowOffset: {width: -2, height: 4},
+    shadowOpacity: 0.2,
+    shadowRadius: 3,
+  },
+  tag: {
+    backgroundColor: '#a9a9a9',
+    width: 60,
+    paddingHorizontal:3,
+    borderRadius: 8
+  },
+  scrollView: {
+    backgroundColor: 'white',
+    marginHorizontal: 7,
   },
 });
