4891b63d5fd508a83eaee6d0b4053cdfcc564d57
Taeho <taehokimbusines@gmail.com>
2023-06-02 11:20:12 +0100

    Update test suite for back-end endpoint

--- backend/tests/test_lists.py
+++ backend/tests/test_lists.py
@@ -3,7 +3,6 @@ from src.main import app
 
 client = TestClient(app)
 
-
-def test_read_main():
+def test_search_end_point():
     response = client.get("/api/v1/search/")
     assert response.status_code == 200
