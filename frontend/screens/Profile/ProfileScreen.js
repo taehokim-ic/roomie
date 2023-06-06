c8ea11e54c84039c9bba568de1285b95cf33b964
Rhea Garud <rg921@ic.ac.uk>
2023-06-06 16:21:05 +0100

    Display some personal details of the user on the profile page

--- frontend/screens/Profile/ProfileScreen.js
+++ frontend/screens/Profile/ProfileScreen.js
@@ -1,7 +1,15 @@
 import React from 'react';
-import { View, Text, Image, StyleSheet, SafeAreaView } from 'react-native';
+import { View, Text, Image, StyleSheet, SafeAreaView, TextInput } from 'react-native';
 
 export default function ProfileScreen() {
+  const user = {
+    name: 'Jihoon W.',
+    age: 22,
+    number: '+44 7705707370',
+    email: 'jihoon.w123@gmail.com',
+    dob: '10.02.2001',
+  }
+
   return (
     <SafeAreaView style={{ flex: 1 }}>
     <Text style={styles.title}>Profile</Text>
@@ -10,8 +18,25 @@ export default function ProfileScreen() {
         source={require('../../assets/profile-picture.png')}
         style={styles.profileImage}
       />
-      <Text style={styles.name}>John Doe</Text>
-      <Text style={styles.bio}>24</Text>
+      <View style={styles.textContainer}>
+        <Text style={styles.name}>{user.name}</Text>
+        <Text style={styles.bio}>{user.age}</Text>
+      </View>
+    </View>
+    <View>
+      <Text style={styles.subtitle}>Personal Details</Text>
+        <View style={styles.fieldContainer}>
+          <Text style={styles.field}>Contact Number    {user.number}</Text>
+        </View>
+        <View style={styles.fieldContainer}>
+          <Text style={styles.field}>Email Address         {user.email}</Text>
+        </View>
+        <View style={styles.fieldContainer}>
+          <Text style={styles.field}>Date of Birth           {user.dob}</Text>
+        </View>
+    </View>
+    <View>
+      <Text style={styles.subtitle}>Legal</Text>
     </View>
     </SafeAreaView>
   );
@@ -19,13 +44,21 @@ export default function ProfileScreen() {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    flexDirection: 'row',
     alignItems: 'center',
   },
   title: {
     fontSize: 32,
-    paddingLeft: 20,
+    paddingLeft: 30,
     paddingTop: 20,
+    fontWeight: 'bold',
+    textAlign: 'left'
+  },
+  subtitle: {
+    fontSize: 20,
+    paddingLeft: 30,
+    paddingTop: 20,
+    paddingBottom: 20,
     fontWeight: 'bold',
     textAlign: 'left'
   },
@@ -33,7 +66,10 @@ const styles = StyleSheet.create({
     width: 150,
     height: 150,
     borderRadius: 75,
-    marginBottom: 20,
+    marginRight: 30,
+    marginLeft: 20,
+    marginBottom: 15,
+    marginTop: 25
   },
   name: {
     fontSize: 24,
@@ -42,4 +78,19 @@ const styles = StyleSheet.create({
   bio: {
     fontSize: 16,
   },
+  input: {
+    padding: 8,
+    marginBottom: 16,
+  },
+  field: {
+    marginRight: 12,
+    fontSize: 15,
+    marginLeft: 49,
+    marginBottom: 10,
+    marginTop: 10,
+  },
+  fieldContainer: {
+    borderWidth: 1,
+    borderColor: '#ccc',
+  }
 });
