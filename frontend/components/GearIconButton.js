3f2398517d4b9b7b2d1f0601335fc9ab8bbe8f9d
aj2421 <arnav.jadhav21@imperial.ac.uk>
2023-06-07 15:40:32 +0100

    Added 2 filter pages and separate buttons

--- frontend/components/GearIconButton.js
+++ frontend/components/GearIconButton.js
@@ -1,24 +1,19 @@
-import React, { useState } from 'react';
-import { TouchableOpacity, StyleSheet } from 'react-native';
-import { Ionicons } from '@expo/vector-icons';
+import React from 'react';
+import { TouchableOpacity, View, Text, StyleSheet } from 'react-native';
+import { FontAwesome } from '@expo/vector-icons';
 
-const GearIconButton = () => {
-  const [color, setColor] = useState('black');
-
-  const handlePress = () => {
-    setColor(prevColor => prevColor === 'black' ? 'red' : 'black');
-  };
-
+const GearIconButton = ({ onPress }) => {
   return (
-    <TouchableOpacity style={styles.button} onPress={handlePress}>
-      <Ionicons name="ios-settings" size={32} color={color} />
+    <TouchableOpacity style={styles.iconContainer} onPress={onPress}>
+      <FontAwesome name="gear" size={26} color="black" />
     </TouchableOpacity>
   );
 };
 
 const styles = StyleSheet.create({
-  button: {
-    padding: 10,
+  iconContainer: {
+    marginTop: 11,
+    marginLeft: 316,
   },
 });
 
--- frontend/navigation/TabsNavigation.js
+++ frontend/navigation/TabsNavigation.js
@@ -20,7 +20,7 @@ const Tab = createBottomTabNavigator();
 
 const getTabBarVisibility = (route) => {
     let rn = getFocusedRouteNameFromRoute(route);
-    if (rn == 'SearchFilter') return 'none';
+    if (rn == 'MainFilter' || rn == 'SearchFilter') return 'none';
     return 'flex'
 }
 
