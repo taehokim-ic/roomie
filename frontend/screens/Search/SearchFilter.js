46e9db3fac8c11916994f693f0fdd87967ece431
aj2421 <arnav.jadhav21@imperial.ac.uk>
2023-06-07 19:24:32 +0100

    Added a trial dropdown box, need to copy and paste this, as well as add API calls for it

--- frontend/screens/Search/SearchFilter.js
+++ frontend/screens/Search/SearchFilter.js
@@ -4,6 +4,8 @@ import { View, StyleSheet } from 'react-native';
 
 import SearchButton from '../../components/SearchButton';
 import GearIconButton from '../../components/GearIconButton';
+import SmokerDropdown from '../../components/SmokerDropdown';
+
 
 const SearchFilter = () => {
   const navigation = useNavigation();
@@ -20,6 +22,7 @@ const SearchFilter = () => {
     <View style={styles.container}>
       <SearchButton onPress={handleSearchButtonPress} title="Enter location"/>
       <GearIconButton onPress={handleGearIconButtonPress} color='#03c9a9'/>
+      <SmokerDropdown />
     </View>
   )
 };
