b71dd023fb905834fb1e08174e7cd9bf512738c7
Mithun Srinavaratnarajah <mithun@Mithuns-MacBook-Pro.local>
2023-06-09 00:10:21 +0100

    Implement retreiving user profile data from database

--- frontend/App.js
+++ frontend/App.js
@@ -1,28 +1,22 @@
 import * as React from 'react';
+import { useState, useEffect } from 'react';
 import TabsNavigation from './navigation/TabsNavigation';
 import UserContext from './context/UserContext';
+import axios from 'axios';
 
 const App = () => {
-    const user = {
-        id: '123',
-        name: 'John Doe',
-        profile_url: 'https://picsum.photos/200',
-        bio: 'I am a cool person',
-        requirements: 'I am looking for a cool person',
-        additional_info: 'I am a cool person',
-        email: 'example@example.com',
-        age: 21,
-        dob: '01/01/2000',
-        nationality: 'Singaporean',
-        preferredLocation: 'London',
-        gender: 'Male',
-        smoker: 'No',
-        hasPets: 'No',
-        minBudget: 1000,
-        maxBudget: 2000,
-        primaryLanguage: 'English',
-        contactNumber: '12345678',
-      };
+
+    const [user, setUser] = useState(null);
+
+    useEffect(() => {
+        const fetchUser = async () => {
+            const response = await axios.get('http://roomie3.herokuapp.com/api/v1/profile/d274e899-53a6-4b25-8558-e28749032a4f');
+            console.log(response.data);
+            setUser(response.data);
+        };
+
+        fetchUser();
+    }, []);
 
     return (
         <UserContext.Provider value={user}>
