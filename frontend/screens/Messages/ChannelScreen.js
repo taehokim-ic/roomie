4065b13d31a0c96c4186655e6d707babb4408b55
Mithun Srinavaratnarajah <mithun@dyn3170-34.wlan.ic.ac.uk>
2023-06-15 15:55:28 +0100

    Implement chat room with stream IO APK

--- frontend/screens/Messages/ChannelScreen.js
+++ frontend/screens/Messages/ChannelScreen.js
@@ -1,15 +1,18 @@
 import {View, Text, SafeAreaView} from 'react-native';
 import React from 'react';
 import {useChatContext} from '../../context/ChatContext';
+import { ChannelList } from 'stream-chat-expo';
 
-const ChannelScreen = () => {
-    const {username} = useChatContext();
+const ChannelScreen = ({navigation}) => {
+    const {setCurrentChannel} = useChatContext();
+
+    const onSelect = (channel) => {
+        setCurrentChannel(channel);
+        navigation.navigate('ChatRoom');
+    }
+
     return (
-        <SafeAreaView>
-        <View>
-            <Text>{username}</Text>
-        </View>
-        </SafeAreaView>
+        <ChannelList onSelect={onSelect}/>
     )
 }
 
