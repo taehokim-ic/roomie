33d95644c2c43d9e93ddeff543d5db82f271edec
Mithun Srinavaratnarajah <mithun@dyn3175-211.wlan.ic.ac.uk>
2023-06-19 15:43:40 +0100

    Update UUIDs

--- frontend/context/uuid.js
+++ frontend/context/uuid.js
@@ -1,12 +1,8 @@
 import { v4 as uuidv4 } from 'uuid';
 
 export function generateUUID() {
+    const uuid1 = '54c2b36a-41ef-4c72-9f83-60c4bdf3fcf7';
     const uuid2 = '05b3bbd1-4e75-4ad3-9d71-4c4c8d08717d';
-    const uuid3 = '8b92c64a-6e82-4592-9419-7d70a9a7f85e';
 
-    const uuid1 = 'a27f8d9a-268d-4e14-99ab-ebf1e240784e';
-    const uuid4 = '2af140f6-3906-4f3c-8439-55c1c7d3a0ce';
-    const uuid5 = '4dbcdde5-673b-40e0-8727-f52734f0a4d9';
-
-    return uuid2;
+    return uuid1;
 }
